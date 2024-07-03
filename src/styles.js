f928864412adbd344b0b225a05456531722f156a
Sam <samuel@encodevlabs.com>
2024-07-03 06:14:59 +0000

    added styles

---
+++ src/styles.js
@@ -0,0 +1,18 @@
+export const buttone = {
+ alignItems: "center",
+ padding: 10,
+ width: "10%",
+ backgroundColor: "tomato",
+ borderRadius: 10,
+};
+
+export const item = {
+ display: "flex",
+ justifyContent: "space-between",
+ flexDirection: "row",
+ padding: 10,
+ marginTop: 10,
+ borderWidth: 3,
+ marginHorizontal: 10,
+ alignItems: "center",
+};
