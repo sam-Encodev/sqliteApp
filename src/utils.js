923b28ece7927add101918552e27ef33cbe844ae
Sam <samuel@encodevlabs.com>
2024-07-03 06:15:53 +0000

    code lint

--- src/utils.js
+++ src/utils.js
@@ -3,38 +3,38 @@ import { db } from "../db";
 import { user } from "../db/schema";
 
 const getUsers = async () => {
-  await db.select().from(user);
+ await db.select().from(user);
 };
 
 const deleteUsers = async () => {
-  await db.delete(user);
+ await db.delete(user);
 };
 
 const deleteUser = async (id) => {
-  await db.delete(user).where(eq(user.id, id));
+ await db.delete(user).where(eq(user.id, id));
 };
 
 const getUser = async (id) => {
-  await db.select().from(user).where(eq(user.id, id));
+ await db.select().from(user).where(eq(user.id, id));
 };
 
 const countUsers = async () => {
-  await db.select({ count: count() }).from(user);
+ await db.select({ count: count() }).from(user);
 };
 
 const addUser = async ({ name, email }) => {
-  await db.insert(user).values({
-    name: name,
-    email: email,
-    createdAt: Date.now(),
-    updatedAt: Date.now(),
-  });
+ await db.insert(user).values({
+  name: name,
+  email: email,
+  createdAt: Date.now(),
+  updatedAt: Date.now(),
+ });
 
-  getUsers();
+ getUsers();
 };
 
 const updateUser = async (name) => {
-  await db.update(user).set({ name: "Mr. Dan" }).where(eq(user.name, name));
+ await db.update(user).set({ name: "Mr. Dan" }).where(eq(user.name, name));
 };
 
 export { addUser };
