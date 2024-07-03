8463672a1e813a28d390afbeaa15f6ceb50ef412
Sam <samuel@encodevlabs.com>
2024-07-03 06:14:49 +0000

    component cleanup

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
 
 export default function EmptyState() {
-  return <Text style={styles.headerText}>EmptyState</Text>;
+ return <Text style={{ textAlign: "center" }}>EmptyState</Text>;
 }
-
-const styles = StyleSheet.create({
-  headerText: {
-    textAlign: "center",
-  },
-});
