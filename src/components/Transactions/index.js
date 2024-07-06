cb5ad88458cdd2d85afa644803dc5d8e29e0b476
Sam <samuel@encodevlabs.com>
2024-07-06 19:34:07 +0000

    added ActionSheetIOS

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -6,26 +6,62 @@ import EmptyState from "../EmptyState";
 import TransactionSummary from "./Summary";
 import { transaction } from "../../../db/schema";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
-import { TouchableOpacity, View, FlatList } from "react-native";
+import {
+ TouchableOpacity,
+ View,
+ FlatList,
+ ActionSheetIOS,
+ Platform,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function TransactionList() {
+ const navigation = useNavigation();
  const { data } = useLiveQuery(db.select().from(transaction));
 
  const deleteTransaction = async (id) => {
   await db.delete(transaction).where(eq(transaction.id, id));
  };
 
+ //  const [selecetedIndex, setSelectedIndex] = useState(null);
+
+ const _handelActions = () => {
+  ActionSheetIOS.showActionSheetWithOptions(
+   {
+    options: ["Edit", "Delete", "Cancel"],
+    destructiveButtonIndex: 1,
+    cancelButtonIndex: 2,
+   },
+   (buttonIndex) => {
+    if (buttonIndex === 1) {
+     deleteTransaction(data.item.id);
+    }
+   },
+  );
+ };
+
  return (
   <View style={{ height: "100%" }}>
    <FlatList
     data={data}
-    renderItem={(data) => (
-     <View key={data.item.id}>
+    renderItem={({ item, index }) => (
+     <View key={item?.id}>
       <TouchableOpacity
        activeOpacity={0.7}
-       onLongPress={() => deleteTransaction(data.item.id)}
+       onPress={() => {
+        // setSelectedIndex(index);
+        if (Platform.OS === "ios") {
+         return _handelActions();
+        }
+
+        navigation.navigate({
+         name: "Sheet",
+         params: { index },
+        });
+       }}
+       onLongPress={() => console.log("onLongPress")}
       >
-       <ListItem transaction={data.item} />
+       <ListItem transaction={item} />
       </TouchableOpacity>
      </View>
     )}
