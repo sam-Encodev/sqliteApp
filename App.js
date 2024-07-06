daa18e4558323d7f5e9936f14a0f1cbbe7999583
Sam <samuel@encodevlabs.com>
2024-07-06 19:34:19 +0000

    added tamagui

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import Main from "./src/Main";
+import { config } from "@tamagui/config/v3";
 import { StatusBar } from "expo-status-bar";
+import { TamaguiProvider, createTamagui } from "tamagui";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+const appConfig = createTamagui(config);
 export default function App() {
  return (
   <NavigationContainer>
-   <GestureHandlerRootView style={{ flex: 1 }}>
-    <StatusBar style="auto" />
-    <Main />
-   </GestureHandlerRootView>
+   <TamaguiProvider config={appConfig}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+     <StatusBar style="auto" />
+     <Main />
+    </GestureHandlerRootView>
+   </TamaguiProvider>
   </NavigationContainer>
  );
 }
