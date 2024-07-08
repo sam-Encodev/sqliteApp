e0cdcb6651505e1f81c0474d942a289ac2a11bbf
Sam <samuel@encodevlabs.com>
2024-07-08 16:09:36 +0000

    db migration

--- db/migrations/migrations.js
+++ db/migrations/migrations.js
@@ -2,10 +2,18 @@
 
 import journal from "./meta/_journal.json";
 import m0000 from "./0000_dizzy_kate_bishop.sql";
+import m0001 from "./0001_simple_clea.sql";
+import m0002 from "./0002_flimsy_texas_twister.sql";
+import m0003 from "./0003_typical_impossible_man.sql";
+import m0004 from "./0004_outstanding_frank_castle.sql";
 
 export default {
  journal,
  migrations: {
   m0000,
+  m0001,
+  m0002,
+  m0003,
+  m0004,
  },
 };
--- db/schema.js
+++ db/schema.js
@@ -13,9 +13,11 @@ export const transaction = sqliteTable("transactions", {
  id: integer("id").primaryKey({ autoIncrement: true }),
  category_id: integer("category_id").notNull(),
  amount: real("amount").notNull(),
- date: text("date").default(sql`(CURRENT_TIMESTAMP)`),
- description: text("description"),
+ date: text("date").notNull().default(sql`(current_date)`),
+ description: text("description").notNull(),
  type: text("type", { enum: ["Expense", "Income"] }).notNull(),
- createdAt: integer("created_at"),
- updatedAt: integer("updated_at"),
+ createdAt: integer("created_at").notNull().default(sql`(current_timestamp)`),
+ updatedAt: integer("updated_at")
+  .notNull()
+  .$onUpdateFn(() => sql`(current_timestamp)`),
 });
