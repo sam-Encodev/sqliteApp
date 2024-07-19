cebbdefd08212ce03ea10319ad7c3baf9a7c0afa
Sam <samuel@encodevlabs.com>
2024-07-19 15:32:45 +0000

    chore: Update babel.config.js to use async function syntax

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-export default function (api) {
- api.cache(true);
- return {
-  presets: ["babel-preset-expo"],
-  plugins: [["inline-import", { extensions: [".sql"] }]],
- };
-};
+module.exports = function async (api) {
+    api.cache(true);
+    return {
+      presets: ['babel-preset-expo'],
+      plugins: [["inline-import", { extensions: [".sql"] }]],
+    };
+  };
